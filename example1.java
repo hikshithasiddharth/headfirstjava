dc55e1a9ca19b679cd6ad6927f646003573c2274
Hikshitha Siddharth <hikshithasiddharth@Siddharths-MacBook-Air.local>
2025-05-02 16:03:07 +0530

    example1 added

---
+++ example1.java
@@ -0,0 +1,13 @@
+class Example1{
+    public static void main (String[]args){
+        //integer type
+           int age = 2;
+        //float type
+           float height = 1.5f;
+        //String type
+           String name = "loki";
+    System.out.println("Name:"+name);
+    System.out.println("Age:"+age);
+    System.out.println("Height:"+height);
+     }
+}  
