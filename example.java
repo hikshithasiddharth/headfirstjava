b0bebbe995ccfc509073ca4949bce8482f41d826
Hikshitha Siddharth <hikshithasiddharth@Siddharths-MacBook-Air.local>
2025-05-02 15:51:57 +0530

    added example

---
+++ example.java
@@ -0,0 +1,13 @@
+class Example{
+    public static void main(String[]args){
+        //integer type
+          int age = 25;
+        // Boolean type
+          boolean isStudying = true;
+        //String type 
+          String name = "Hikshitha";
+       System.out.println("Name: " + name);
+       System.out.println("Age: " + age);
+       System.out.println("Is Studying: " + isStudying);
+    }
+  }
